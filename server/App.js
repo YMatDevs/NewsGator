af97977cc95d43057572123fea845e536acf2347
Yash Mathur <yashm.devs@gmail.com>
2025-01-04 21:24:11 +0530

    Base Update 1

---
+++ server/App.js
@@ -0,0 +1,40 @@
+// Importing the Express App
+import express from 'express';
+
+// Importing Packages
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import session from 'express-session';
+import { JsonWebTokenError } from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+// Connecting to Database
+mongoose
+    .connect("mongodb://127.0.0.1:27017/NewsGator")
+    .then(() => { console.log("Server Database Connection established")})
+    .catch(() => { console.log("Failed to connect to Database")})
+
+
+// Using Middleware
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('/public'));
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cookieParser());
+
+// Add Passport Authentication
+// Use express Session
+
+
+const app = express();
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+    console.log(`The App is listening on port ${port}`);
+});
